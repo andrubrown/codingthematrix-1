8e3f589b03eaee7cc5b0401c30e361e5e87180f7
Jean-Philippe Barrette-LaPierre <jpb@rrette.com>
2015-03-15 21:20:14 +0800

    ECC lab completed

--- ecc_lab.py
+++ ecc_lab.py
@@ -4,28 +4,45 @@ coursera = 1
 
 from vec import Vec
 from mat import Mat
-from bitutil import bits2mat, str2bits, noise
+from bitutil import bits2mat, str2bits, noise, bits2str, mat2bits
 from GF2 import one
+from matutil import *
+from vecutil import *
 
 ## Task 1
 """ Create an instance of Mat representing the generator matrix G. You can use
 the procedure listlist2mat in the matutil module (be sure to import first).
 Since we are working over GF (2), you should use the value one from the
 GF2 module to represent 1"""
-G = None
+G = listlist2mat([[one,0,one,one],
+                  [one,one,0,one],
+                  [0,0,0,one],
+                  [one,one,one,0],
+                  [0,0,one,0],
+                  [0,one,0,0],
+                  [one,0,0,0]])
 
 ## Task 2
 # Please write your answer as a list. Use one from GF2 and 0 as the elements.
-encoding_1001 = None
+message_1001 = list2vec([one,0,0,one])
+#encoding_1001 = G*message_1001
+encoding_1001 = [0,0,one,one,0,0,one]
 
 
 ## Task 3
 # Express your answer as an instance of the Mat class.
-R = None
+R = Mat((set(range(4)),set(range(7))), {(0,6):one, (1,5):one, (2,4):one, (3,2):one})
+t3_codeword = list2vec([0,one,one,one,one,0,0])
+R*t3_codeword
+
 
 ## Task 4
 # Create an instance of Mat representing the check matrix H.
-H = None
+H = listlist2mat([[0,0,0,one,one,one,one],
+                  [0,one,one,0,0,one,one],
+                  [one,0,one,0,one,0,one]])
+
+#print(H*G)
 
 ## Task 5
 def find_error(syndrome):
@@ -42,14 +59,21 @@ def find_error(syndrome):
         >>> find_error(Vec({0,1,2}, {})) == Vec({0,1,2,3,4,5,6}, {})
         True
     """
-    pass
+    assert(syndrome.D == H.D[0])
+    if syndrome == zero_vec(syndrome.D):
+        return Vec(H.D[1], {})
+
+    val = -1
+    for i in range(3):
+        val += pow(2, i)*(1 if (syndrome[2-i] == one) else 0)
+    return Vec(H.D[1], {val:one})
 
 ## Task 6
 # Use the Vec class for your answers.
 non_codeword = Vec({0,1,2,3,4,5,6}, {0: one, 1:0, 2:one, 3:one, 4:0, 5:one, 6:one})
-error_vector = Vec(..., ...)
-code_word = Vec(..., ...)
-original = ... # R * code_word
+error_vector = Vec({0,1,2,3,4,5,6}, {6: one}) # find_error(H*non_codeword)
+code_word = Vec({0,1,2,3,4,5,6}, {0:one, 2:one, 3:one, 5:one}) # error_vector + non_codeword
+original = Vec({0, 1, 2, 3},{0: 0, 1: one, 2: 0, 3: one}) # R * code_word
 
 
 ## Task 7
@@ -62,20 +86,23 @@ def find_error_matrix(S):
         >>> find_error_matrix(S) == Mat(({0, 1, 2, 3, 4, 5, 6}, {0, 1, 2, 3}), {(1, 3): 0, (3, 0): 0, (2, 1): 0, (6, 2): 0, (5, 1): one, (0, 3): 0, (4, 0): 0, (1, 2): 0, (3, 3): 0, (6, 3): 0, (5, 0): 0, (2, 2): 0, (4, 1): 0, (1, 1): 0, (3, 2): one, (0, 0): 0, (6, 0): 0, (2, 3): 0, (4, 2): 0, (1, 0): 0, (5, 3): 0, (0, 1): 0, (6, 1): 0, (3, 1): 0, (2, 0): 0, (4, 3): one, (5, 2): 0, (0, 2): 0})
         True
     """
-    pass
+    return coldict2mat({k:find_error(col) for k,col in mat2coldict(S).items()})
+        
 
 ## Task 8
 s = "I'm trying to free your mind, Neo. But I can only show you the door. You're the one that has to walk through it."
-P = None
+P = bits2mat(str2bits(s))
+E = noise(P, 0.02)
+bits2str(mat2bits(P+E))
 
 ## Task 9
-C = None
-bits_before = None
-bits_after = None
-
+C = G*P
+bits_before = 896
+bits_after = 1568
 
 ## Ungraded Task
-CTILDE = None
+CTILDE = C + noise(C, 0.01)
+bits2str(mat2bits(H*CTILDE))
 
 ## Task 10
 def correct(A):
@@ -87,5 +114,8 @@ def correct(A):
         >>> correct(A) == Mat(({0, 1, 2, 3, 4, 5, 6}, {1, 2, 3}), {(0, 1): 0, (1, 2): 0, (3, 2): 0, (1, 3): 0, (3, 3): 0, (5, 2): one, (6, 1): 0, (3, 1): 0, (2, 1): 0, (0, 2): one, (6, 3): one, (4, 2): 0, (6, 2): one, (2, 3): 0, (4, 3): 0, (2, 2): 0, (5, 1): 0, (0, 3): one, (4, 1): 0, (1, 1): 0, (5, 3): one})
         True
     """
-    pass
+    return A+find_error_matrix(H*A)
+
+
+bits2str(mat2bits(R*correct(CTILDE)))
 
